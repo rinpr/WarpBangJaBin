b4c64f55eb8245c732520e19eefe03b07bb37cc6
rin <duggerseven@gmail.com>
2023-04-10 02:43:01 +0700

    add javadoc to global variable

--- src/main/java/com/rinpr/warpbangjabin/util/TpaRequestStore.java
+++ src/main/java/com/rinpr/warpbangjabin/util/TpaRequestStore.java
@@ -6,17 +6,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TpaRequestStore {
+    /**
+     * Store tpaRequest from requester to target.
+     */
     private static Map<Player, Player> tpaRequest = new HashMap<>();
 
-    public static void addTpaRequest(Player from, Player to) {
-        tpaRequest.put(from, to);
-    }
+    /**
+     * This static method use to add tpa request to global variables.
+     * @param from requester.
+     * @param to target.
+     */
+    public static void addTpaRequest(Player from, Player to) { tpaRequest.put(from, to); }
 
-    public static void removeTpaRequest(Player from) {
-        tpaRequest.remove(from);
-    }
+    /**
+     * This static method use to remove tpa request by requester.
+     * @param from requester.
+     */
+    public static void removeTpaRequest(Player from) { tpaRequest.remove(from); }
 
-    public static Player getTpaRequest(Player from) {
-        return tpaRequest.get(from);
-    }
+    /**
+     * This static method use to get tpa request by requester.
+     * @param from requester.
+     */
+    public static Player getTpaRequest(Player from) { return tpaRequest.get(from); }
 }
