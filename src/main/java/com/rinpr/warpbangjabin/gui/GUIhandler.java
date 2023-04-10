7f60708b352dbbc93feeeacff17262d06dd24865
rin <duggerseven@gmail.com>
2023-04-10 20:03:10 +0700

    change variable name player_list to player_slot for easy recognize

--- src/main/java/com/rinpr/warpbangjabin/gui/GUIhandler.java
+++ src/main/java/com/rinpr/warpbangjabin/gui/GUIhandler.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class GUIhandler {
     private Player player;
     private final int[] space_slot = new int[]{0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,45,46,47,49,51,52,53};
-    private final int[] player_list = new int[]{10,11,12,13,14,15,16,19,20,21,22,23,24,25,28,29,30,31,32,33,34,37,38,39,40,41,42,43};
-    private final int playersPerPage = player_list.length;
+    private final int[] player_slot = new int[]{10,11,12,13,14,15,16,19,20,21,22,23,24,25,28,29,30,31,32,33,34,37,38,39,40,41,42,43};
+    private final int playersPerPage = player_slot.length;
     public GUIhandler(Player player) {
         this.player = player;
     }
@@ -35,7 +35,7 @@ public class GUIhandler {
         int startIndex = (pageNum - 1) * playersPerPage;
         int endIndex = Math.min(startIndex + playersPerPage, active_player.size());
         for (int i = startIndex; i < endIndex; i++) {
-            inv.setItem(player_list[i - startIndex], active_player.get(i));
+            inv.setItem(player_slot[i - startIndex], active_player.get(i));
         }
         player.openInventory(inv);
     }
@@ -46,7 +46,7 @@ public class GUIhandler {
         for (int slot : space_slot) { inv.setItem(slot,fromConfig.getSpaceSlot()); }
         List<ItemStack> active_player = PlayerSkull.getOnlinePlayerSkull();
         for (int i = 0; i < active_player.size(); i++) {
-            inv.setItem(player_list[i], active_player.get(i));
+            inv.setItem(player_slot[i], active_player.get(i));
         }
         player.openInventory(inv);
     }
