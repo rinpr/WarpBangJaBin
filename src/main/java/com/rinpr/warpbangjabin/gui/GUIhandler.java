4ee8cc0b96817957c3d61b8ee07bd1ff39d8e336
rin <duggerseven@gmail.com>
2023-04-10 20:14:34 +0700

    add javadoc and fix openTPAgui method

--- src/main/java/com/rinpr/warpbangjabin/gui/GUIhandler.java
+++ src/main/java/com/rinpr/warpbangjabin/gui/GUIhandler.java
@@ -9,24 +9,23 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
+/**
+ * This class handling gui creation for this plugin. This contains open teleport gui and
+ * open teleport request method.
+ */
 public class GUIhandler {
-    private Player player;
+    private final Player player;
     private final int[] space_slot = new int[]{0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,45,46,47,49,51,52,53};
     private final int[] player_slot = new int[]{10,11,12,13,14,15,16,19,20,21,22,23,24,25,28,29,30,31,32,33,34,37,38,39,40,41,42,43};
     private final int playersPerPage = player_slot.length;
     public GUIhandler(Player player) {
         this.player = player;
     }
+
+    /** This public method use to open teleport gui for player with given page.
+     * @param pageNum page number you want to open for this gui
+     */
     public void openTPgui(int pageNum) {
-//        Inventory inv = Bukkit.createInventory(player, 54, fromConfig.getPlayerTitle());
-//        inv.setItem(48, fromConfig.getPreviousButton());
-//        inv.setItem(50, fromConfig.getNextButton());
-//        for (int slot : space_slot) { inv.setItem(slot,fromConfig.getSpaceSlot()); }
-//        List<ItemStack> active_player = PlayerSkull.getOnlinePlayerSkull();
-//        for (int i = 0; i < active_player.size(); i++) {
-//            inv.setItem(player_list[i], active_player.get(i));
-//        }
-//        player.openInventory(inv);
         Inventory inv = Bukkit.createInventory(player, 54, fromConfig.getPlayerTitle());
         inv.setItem(48, fromConfig.getPreviousButton());
         inv.setItem(50, fromConfig.getNextButton());
@@ -39,14 +38,20 @@ public class GUIhandler {
         }
         player.openInventory(inv);
     }
-    public void openTPAgui() {
+
+    /** This public method use to open teleport request gui for player with given page.
+     * @param pageNum page number you want to open for this gui
+     */
+    public void openTPAgui(int pageNum) {
         Inventory inv = Bukkit.createInventory(player, 54, fromConfig.getPlayerTitle() + " ");
         inv.setItem(48, fromConfig.getPreviousButton());
         inv.setItem(50, fromConfig.getNextButton());
         for (int slot : space_slot) { inv.setItem(slot,fromConfig.getSpaceSlot()); }
         List<ItemStack> active_player = PlayerSkull.getOnlinePlayerSkull();
-        for (int i = 0; i < active_player.size(); i++) {
-            inv.setItem(player_slot[i], active_player.get(i));
+        int startIndex = (pageNum - 1) * playersPerPage;
+        int endIndex = Math.min(startIndex + playersPerPage, active_player.size());
+        for (int i = startIndex; i < endIndex; i++) {
+            inv.setItem(player_slot[i - startIndex], active_player.get(i));
         }
         player.openInventory(inv);
     }
